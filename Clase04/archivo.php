f177faa017f5f5a72275d1c72dc73609f4822ed0
alumno <alumno>
2017-04-10 20:36:07 -0300

    aiuda

---
+++ Clase04/archivo.php
@@ -0,0 +1,16 @@
+<?php
+$file = $_FILES["btnGuardarArchivo"]["name"];
+$target = "uploads/";
+$target_file = $target . $file;
+$target_fileEncontrado;
+//var_dump($_FILES);
+if(file_exists($target_file))
+{
+    copy($target_file , $target. date("Y-m-d-h-i-s"). $file);
+    move_uploaded_file($_FILES["btnGuardarArchivo"]["tmp_name"], "uploads/". $_FILES["btnGuardarArchivo"]["name"]);   
+}
+else 
+{
+    move_uploaded_file($_FILES["btnGuardarArchivo"]["tmp_name"], "uploads/". $_FILES["btnGuardarArchivo"]["name"]);    
+}
+?>
